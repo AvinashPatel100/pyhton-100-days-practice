d4123b9cdea0920f29fe03853f2d0ccefa05c10c
Avinash <114744018+AvinashPatel100@users.noreply.github.com>
2023-10-01 00:15:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+num1 = float(input("enter the first number : "))
+num2 = float(input("enter the second number: "))
+
+print("original number : num1 = ",num1, "num2 = ",num2)
+
+temp = num1
+num1 = num2
+num2 =temp
+
+print("swapped number : num1 = " , num1, "num2= ",num2)
